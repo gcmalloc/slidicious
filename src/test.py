9d6640809bd1f66cb1c9a91113728197f1ff40b2
gcmalloc <gcmalloc@gmail.com>
2013-10-25 18:56:59 +0200

    test the new build directory

--- src/test.py
+++ src/test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+import os
 import shutil
 import task
 import app
 import unittest
+import tempfile
 
 class AppTest(unittest.TestCase):
 
@@ -10,24 +12,25 @@ class AppTest(unittest.TestCase):
     def __init__(self, arg):
     """
     def setUp(self):
-        #self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        #flaskr.app.config['TESTING'] = True
         self.app = app.app.test_client()
-        #flaskr.init_db()
 
     def test_(self):
         resp = self.app.post('/hook', data={'repository':'google.ch'})
 	self.assertEqual(resp.status_code, 200)
 
     def test_root(self):
-        resp = self.app.get('/', {})
-	self.assertEqual(resp.status_code, 200)
+        resp = self.app.get('/', payload={'payload':{}})
+	self.assertEqual(resp.status_code, 400)
 
 
 class TestSlideBuild(unittest.TestCase):
 
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
+        self.temp_template = os.path.join(self.temp_dir, "555")
+
     def test_pdf(self):
-        ret = task.compile_slides("https://github.com/gcmalloc/fabric_presentation.git", build_index=555)
+        ret = task.compile_slides("https://github.com/gcmalloc/fabric_presentation.git", self.temp_dir)
         self.tmp_dir = ret.output_dir
         self.assertIsInstance(ret, task.CompilationOutput)
         self.assertEqual(ret.html, '555.html')
